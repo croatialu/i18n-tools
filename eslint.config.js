0202be6476fd2daad502c12d4c698bff91264f7f
croatialu <wuli.croatia@foxmail.com>
2024-10-22 20:20:05 +0800

    chore: fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,4 +5,11 @@ export default antfu(
   {
     type: 'lib',
   },
+  {
+    files: ['fixtures/**/*.yaml'],
+    rules: {
+      'yml/no-irregular-whitespace': 'off',
+      'no-irregular-whitespace': 'off',
+    },
+  },
 )
--- src/utils/config.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it } from 'vitest'
-import { loadConfig } from './config'
-
-describe('[utils]config', () => {
-  it('should load config', async () => {
-    const config = await loadConfig()
-  })
-})
